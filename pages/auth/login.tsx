0565546c7b6e6fa44a118c14c97b441390499907
STARBREEDERS / Beamit AI <92543099+starbreeders-singularity-dao@users.noreply.github.com>
2024-07-20 17:06:53 +0700

    Update login.tsx

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Swal from "sweetalert2";
 import assest from "@/json/assest";
 import Wrapper from "@/layout/wrapper/Wrapper";
